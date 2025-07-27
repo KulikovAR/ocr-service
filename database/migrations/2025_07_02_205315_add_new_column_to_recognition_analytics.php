98e9fd50a3e68ee2cdcd6649638ea9adfbd1d798
KulikovAR <106818844+SashkaBackEnd@users.noreply.github.com>
2025-07-27 23:24:53 +0300

    nullable

--- database/migrations/2025_07_02_205315_add_new_column_to_recognition_analytics.php
+++ database/migrations/2025_07_02_205315_add_new_column_to_recognition_analytics.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('recognition_analytics', function (Blueprint $table) {
-            $table->timestamp('request_date');
+            $table->timestamp('request_date')->nullable();
         });
     }
 
